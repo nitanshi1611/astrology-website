872f84fc1c2a0049c1ccbe9eaf06a85f14744dd3
nitansi <nitanshi1116@gmail.com>
2024-07-14 00:53:26 +0530

    adding all files

---
+++ fetchFooter.js
@@ -0,0 +1,9 @@
+function fetchFooter() {
+    fetch('./footer.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('footer').innerHTML = data;
+        })
+        .catch(error => console.error('Error loading footer:', error));
+}
+fetchFooter();
